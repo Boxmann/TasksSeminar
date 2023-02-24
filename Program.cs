747256e250be4702a720e3fa25614b46b22b1b57
Student <igorzim97@yandex.ru>
2023-02-24 21:41:59 +0300

    ДЗ урок 6 без доп задания

--- Program.cs
+++ Program.cs
@@ -407,6 +407,8 @@ for (int i = 0; i < array.Length; i++)
 }
 Console.WriteLine($"Количество чисел больше 0:  {count}");
 */
+
+//Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
 Console.Clear();
 Console.Write("Введите k1: ");
 double  k1 = Convert.ToDouble(Console.ReadLine());
